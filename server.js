a84ff8c53dd18a1420af9ad77bb4e2d04cbc00a1
claudia <claudialay41@gmail.com>
2020-08-13 01:20:49 -0500

    corrected seed and schema files

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const sqlite3 = require('sqlite3').verbose();
 
 // express middleware 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = new sqlite3.Database('./db/schema.sql', err => {
+    if (err) {
+      return console.error(err.message);
+    }
+  
+    console.log('Connected to the election database.');
+  });
+
+db.all(`SELECT * FROM candidates`, (err, rows) => 
+{
+    console.log(rows);
+});
 
 // default response for any other request (Not Found) Catch all
 app.use ((req, res) => 
 {
     res.status(404).end();
 });
+
 // // get route - connection to express.js server
 // app.get('/', (req, res) => 
 // {
@@ -20,9 +36,9 @@ app.use ((req, res) =>
 //     });
 // });
 
-
-
-app.listen(PORT, () => 
-{
-    console.log(`Server running on port ${PORT}`);
-})
+// Start server after DB connection
+db.on('open', () => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  });
