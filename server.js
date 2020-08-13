c13b1fd13fa80a9e463d46f9fde3aa6a06f67ea2
claudia <claudialay41@gmail.com>
2020-08-13 02:50:45 -0500

    read and delete endpoints

--- server.js
+++ server.js
@@ -1,18 +1,87 @@
 const express = require('express');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
+
 const sqlite3 = require('sqlite3').verbose();
 
 // express middleware 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/', (req, res) => 
+// connect to database
+const db = new sqlite3.Database('./db/election.db', err => 
 {
-  res.json({
-    message: 'Hello World'
+  if (err) 
+  {
+    return console.error(err.message);
+  }
+  console.log('Connected to the election database.');
+})
+
+// Get all candidates
+app.get('/api/candidates', (req, res) => {
+  const sql = `SELECT * FROM candidates`;
+  const params = [];
+  db.all(sql, params, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      message: 'success',
+      data: rows
+    });
   });
 });
+
+// Get single candidate
+app.get('/api/candidate/:id', (req, res) => {
+  const sql = `SELECT * FROM candidates 
+               WHERE id = ?`;
+  const params = [req.params.id];
+  db.get(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      message: 'success',
+      data: row
+    });
+  });
+});
+
+// Delete a candidate
+app.delete('/api/candidate/:id', (req, res) => {
+  const sql = `DELETE FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+  db.run(sql, params, function(err, result) {
+    if (err) {
+      res.status(400).json({ error: res.message });
+      return;
+    }
+
+    res.json({
+      message: 'successfully deleted',
+      changes: this.changes
+    });
+  });
+});
+
+// create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+//               VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+// // ES5 function, not arrow function, to use this
+// db.run(sql, params, function(err, result) {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result, this.lastID);
+// });
 
 // default response for any other request(not found) catach all
 app.use((req, res) =>
@@ -20,9 +89,11 @@ app.use((req, res) =>
   res.status(404).end();
 });
 
-// function to start express.js server on port 3001
-app.listen(PORT, () => 
+// Start server after DB connection
+db.on('open', () => 
 {
-  console.log(`Server running on port ${PORT}`);
+  app.listen(PORT, () => 
+  {
+    console.log(`Server running on port ${PORT}`);
+  });
 });
-
